fbec87dc11b950d7a3aa2d3fc3d0a44057306920
Marvin <marvin.wollbrueck@web.de>
2024-06-25 23:30:19 +0200

    gpio

--- loop_station/src/main/java/com/loop_station/FXMLController.java
+++ loop_station/src/main/java/com/loop_station/FXMLController.java
@@ -7,10 +7,13 @@ Put header here
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.Initializable; // Add this import statement
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 
 public class FXMLController implements Initializable {
     
@@ -21,9 +24,93 @@ public class FXMLController implements Initializable {
     private void btnClickAction(ActionEvent event) {
         lblOut.setText("Hello World!");
     }
+
+    void setLbl(String s){
+        lblOut.setText(s);
+    }
+    @FXML
+    private void handleKeyPressed(KeyEvent event) {
+        switch (event.getCode()) {
+            case Y:
+                System.out.println("Y pressed");
+                break;
+            case N: 
+                System.out.println("N pressed");
+                break;
+            default: {
+                System.out.println("Key pressed: " + event.getCode());
+            }
+        }
+    }
+
+    public void setScene(Scene scene) {
+        // Add key event handler to switch to the game screen
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, this::handleKeyPressed);
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // handleKeyPressed add to listener
+
+/*
+        var pi4j = Pi4J.newAutoContext();  
+        final var button1 = new SimpleButton(pi4j, PIN.D17, Boolean.FALSE);
+        final var button2 = new SimpleButton(pi4j, PIN.D27, Boolean.FALSE);
+
+        button1.onDown(() -> {
+            //setLbl("Button 1 pressed");
+            Platform.runLater(() -> {
+                setLbl("Button 1 pressed");
+                handleKeyPressed(new KeyEvent(KeyEvent.KEY_PRESSED, "Y", "Y", javafx.scene.input.KeyCode.Y, false, false, false, false));
+            });
+            System.out.println("Button 1 pressed");
+            System.out.flush();
+            // make KeyEvent of Y
+            
+        });
+        button1.onUp(() -> {
+            //setLbl("Button 1 released");
+            Platform.runLater(() -> {
+                setLbl("Button 1 released");
+            });
+            System.out.println("Button 1 released");
+            System.out.flush();
+        });
+        button1.whilePressed(() -> {
+            //setLbl("Button 1 is down");
+            Platform.runLater(() -> {
+                setLbl("Button 1 is down");
+            });
+            System.out.println("Button 1 is down");
+            System.out.flush();
+        }, Duration.ofSeconds(1));
+
+        button2.onDown(() -> {
+            //setLbl("Button 2 pressed");
+            Platform.runLater(() -> {
+                setLbl("Button 2 pressed");
+                handleKeyPressed(new KeyEvent(KeyEvent.KEY_PRESSED, "N", "N", javafx.scene.input.KeyCode.N, false, false, false, false));
+            });
+            System.out.println("Button 2 pressed");
+            System.out.flush();
+            // make KeyEvent of N
+            
+        });
+        button2.onUp(() -> {
+            //setLbl("Button 2 released");
+            Platform.runLater(() -> {
+                setLbl("Button 2 released");
+            });
+            System.out.println("Button 2 released");
+            System.out.flush();
+        });
+        button2.whilePressed(() -> {
+            //setLbl("Button 2 is down");
+            Platform.runLater(() -> {
+                setLbl("Button 2 is down");
+            });
+            System.out.println("Button 2 is down");
+            System.out.flush();
+        }, Duration.ofSeconds(1));*/
     }    
 }
--- loop_station/src/main/java/module-info.java
+++ loop_station/src/main/java/module-info.java
@@ -2,6 +2,13 @@ module com.loop_station {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires com.pi4j;
+    requires com.pi4j.plugin.raspberrypi;
+
+    uses com.pi4j.extension.Extension;
+    uses com.pi4j.provider.Provider;
+    requires java.logging;
     opens com.loop_station to javafx.fxml;
     exports com.loop_station;
+    
 }
