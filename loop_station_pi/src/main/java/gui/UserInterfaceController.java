aa25f9c39b737f9db96a7e01bc218287a919d14b
klausxh <klaus.xhoxhi1@gmail.com>
2024-07-06 16:36:23 +0200

    implemented lost game scenario, modified the Question class so that a fun fact/explanation belongs to a certain question

--- loop_station_pi/src/main/java/gui/UserInterfaceController.java
+++ loop_station_pi/src/main/java/gui/UserInterfaceController.java
@@ -25,47 +25,67 @@ public class UserInterfaceController {
     @FXML
     private Text funFactTxt;
     @FXML
+    private Text funFactField;
+    @FXML
     private Button button1;
     @FXML
     private Button button2;
 
     private int currentQuestionIndex = 0;
     private int correctAnswers = 0;
-    private final int requiredCorrectAnswers = 5;
+    private final int requiredCorrectAnswers = 3;
+    private final int totalQuestionsToAsk = 5;
+    private boolean gameEnded = false;
 
     private List<Question> questions = new ArrayList<>(List.of(
-            new Question("Is the sky blue?", "Yes", "No", "Yes"),
-            new Question("Is grass green?", "Yes", "No", "Yes"),
-            new Question("Do cats meow?", "Yes", "No", "Yes"),
-            new Question("Is 5 greater than 3?", "Yes", "No", "Yes"),
-            new Question("Is 2 less than 4?", "Yes", "No", "Yes"),
-            new Question("Is 7 greater than 10?", "Yes", "No", "No"),
-            new Question("Do birds fly?", "Yes", "No", "Yes"),
-            new Question("Is water wet?", "Yes", "No", "Yes"),
-            new Question("Do fish swim?", "Yes", "No", "Yes"),
-            new Question("Is fire hot?", "Yes", "No", "Yes"),
-            new Question("Which is faster?", "Car", "Bicycle", "Car"),
-            new Question("Which is taller?", "Tree", "House", "Tree"),
-            new Question("Which is heavier?", "Elephant", "Mouse", "Elephant")
+            new Question("Is the sky blue?", "Yes", "No", "Yes",
+                    "The sky appears blue because of the way sunlight interacts with Earth's atmosphere."),
+
+            new Question("Is grass green?", "Yes", "No", "Yes",
+                    "Grass is green due to the presence of chlorophyll."),
+
+            new Question("Do cats meow?", "Yes", "No", "Yes",
+                    "Cats meow as a way to communicate with humans."),
+
+            new Question("Is 5 greater than 3?", "Yes", "No", "Yes",
+                    "In basic arithmetic, 5 is greater than 3."),
+
+            new Question("Is 2 less than 4?", "Yes", "No", "Yes",
+                    "In basic arithmetic, 2 is less than 4."),
+
+            new Question("Is 7 greater than 10?", "Yes", "No", "No",
+                    "In basic arithmetic, 7 is less than 10."),
+
+            new Question("Do birds fly?", "Yes", "No", "Yes",
+                    "Most birds have the ability to fly."),
+
+            new Question("Is water wet?", "Yes", "No", "Yes",
+                    "Water makes things wet and is itself wet."),
+
+            new Question("Do fish swim?", "Yes", "No", "Yes",
+                    "Fish swim by moving their tails and fins."),
+
+            new Question("Is fire hot?", "Yes", "No", "Yes",
+                    "Fire is a source of heat."),
+
+            new Question("Which is faster?", "Car", "Bicycle", "Car",
+                    "Cars generally travel faster than bicycles."),
+
+            new Question("Which is taller?", "Tree", "House", "Tree",
+                    "Many trees grow taller than houses."),
+
+            new Question("Which is heavier?", "Elephant", "Mouse", "Elephant",
+                    "Elephants are significantly heavier than mice.")
     ));
 
-    private final List<String> funFacts = List.of(
-            "Honey never spoils.",
-            "Bananas are berries.",
-            "Koalas sleep around 18 hours a day.",
-            "Octopuses have three hearts.",
-            "There are more stars in the universe than grains of sand on Earth.",
-            "A day on Venus is longer than a year on Venus.",
-            "Humans share 50% of their DNA with bananas.",
-            "A group of flamingos is called a 'flamboyance'.",
-            "Bees can recognize human faces.",
-            "Wombat poop is cube-shaped."
-    );
+
+    private List<Question> selectedQuestions;
 
     @FXML
     public void initialize() {
-        // Shuffle the questions list
+        // Shuffle and select 5 questions from the pool
         Collections.shuffle(questions, new Random());
+        selectedQuestions = questions.subList(0, totalQuestionsToAsk);
 
         updateQuestion();
         button1.setOnAction(e -> handleAnswer(button1.getText()));
@@ -79,20 +99,25 @@ public class UserInterfaceController {
     }
 
     private void updateQuestion() {
-        if (currentQuestionIndex < questions.size()) {
-            Question currentQuestion = questions.get(currentQuestionIndex);
+        if (currentQuestionIndex < selectedQuestions.size()) {
+            Question currentQuestion = selectedQuestions.get(currentQuestionIndex);
             questionTxt.setText(currentQuestion.getText());
             button1.setText(currentQuestion.getOptionA());
             button2.setText(currentQuestion.getOptionB());
             correctAnswersCounter.setText(correctAnswers + "/" + requiredCorrectAnswers);
-            funFactTxt.setText(funFacts.get(new Random().nextInt(funFacts.size())));
+            funFactTxt.setText(currentQuestion.getFunFact()); // Display the fun fact associated with the current question
         } else {
-            showRewardScreen();
+            endGame();
         }
     }
 
+
     private void handleAnswer(String answer) {
-        Question currentQuestion = questions.get(currentQuestionIndex);
+        if (gameEnded) {
+            return;
+        }
+
+        Question currentQuestion = selectedQuestions.get(currentQuestionIndex);
         if (answer.equals(currentQuestion.getCorrectAnswer())) {
             correctAnswers++;
         }
@@ -100,35 +125,76 @@ public class UserInterfaceController {
 
         correctAnswersCounter.setText(correctAnswers + "/" + requiredCorrectAnswers);
 
-        if (correctAnswers >= requiredCorrectAnswers) {
-            showRewardScreen();
+        if (currentQuestionIndex >= totalQuestionsToAsk) {
+            endGame();
         } else {
             updateQuestion();
         }
     }
 
     private void handleKeyPressed(KeyEvent event) {
+        if (gameEnded) {
+            switchToScreenSaver();
+            return;
+        }
+
         switch (event.getCode()) {
             case Y:
                 handleAnswer(button1.getText());
                 break;
-            case N: handleAnswer(button2.getText());
+            case N:
+                handleAnswer(button2.getText());
                 break;
             default: {}
         }
     }
 
+    private void endGame() {
+        gameEnded = true;
+        if (correctAnswers >= requiredCorrectAnswers) {
+            showRewardScreen();
+        } else {
+            funFactTxt.setText("Sorry, you lost :(");
+            funFactField.setText("Press any button to go back to the home screen");
+        }
+    }
+
+    private void switchToScreenSaver() {
+        try {
+            Stage stage = (Stage) questionTxt.getScene().getWindow();
+            stage.close();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/ScreenSaver.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1080, 1500);
+
+            Stage screenSaverStage = new Stage();
+            screenSaverStage.setScene(scene);
+            screenSaverStage.getScene().setCursor(Cursor.NONE);
+            screenSaverStage.setTitle("Screen Saver");
+
+            screenSaverStage.setFullScreenExitHint("");
+            screenSaverStage.setFullScreen(true);
+
+            ScreenSaverController controller = loader.getController();
+            controller.setScene(scene);
+
+            screenSaverStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void showRewardScreen() {
         try {
-            // Close the current game stage
             Stage gameStage = (Stage) questionTxt.getScene().getWindow();
             gameStage.close();
-            
+
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/RewardScreen.fxml"));
             Parent root = loader.load();
             RewardScreenController controller = loader.getController();
 
-            Scene scene = new Scene(root, 1080, 1500);
+            Scene scene = new Scene(root, 1080, 1920);
             Stage rewardStage = new Stage();
             rewardStage.setScene(scene);
             rewardStage.getScene().setCursor(Cursor.NONE);
@@ -138,8 +204,6 @@ public class UserInterfaceController {
             rewardStage.setFullScreen(true);
 
             controller.setScene(scene);
-
-            
 
             rewardStage.show();
         } catch (IOException e) {
@@ -152,12 +216,14 @@ public class UserInterfaceController {
         private final String optionA;
         private final String optionB;
         private final String correctAnswer;
+        private final String funFact;
 
-        public Question(String text, String optionA, String optionB, String correctAnswer) {
+        public Question(String text, String optionA, String optionB, String correctAnswer, String funFact) {
             this.text = text;
             this.optionA = optionA;
             this.optionB = optionB;
             this.correctAnswer = correctAnswer;
+            this.funFact = funFact;
         }
 
         public String getText() {
@@ -175,5 +241,10 @@ public class UserInterfaceController {
         public String getCorrectAnswer() {
             return correctAnswer;
         }
+
+        public String getFunFact() {
+            return funFact;
+        }
     }
+
 }
